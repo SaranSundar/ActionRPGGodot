e7c2fbdac5f80532ad82454fa5cf149d1efdcf20
SaranSundar <saransszg@gmail.com>
2021-02-28 11:24:29 -0600

    Added rolling

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 const ACCELERATION = 500
 const MAX_SPEED = 80
+const ROLL_SPEED = 125
 const FRICTION = 500
 
 enum {
@@ -14,6 +15,7 @@ var state = MOVE
 
 var velocity = Vector2.ZERO
 var input_vector = Vector2.ZERO
+var roll_vector = Vector2.LEFT
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
@@ -25,6 +27,9 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
+		return
+	elif Input.is_action_just_pressed("roll"):
+		state = ROLL
 		return
 		
 	input_vector = Vector2.ZERO
@@ -38,7 +43,7 @@ func _physics_process(delta):
 	elif state == ATTACK:
 		attack_state(delta)
 	elif state == ROLL:
-		pass
+		roll_state(delta)
 		
 func attack_state(delta):
 	velocity = Vector2.ZERO
@@ -47,14 +52,28 @@ func attack_state(delta):
 func attack_animation_finished():
 	state = MOVE
 	
+func roll_state(delta):
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move_with_velocity()
+
+func roll_animation_finished():
+	velocity = velocity * 0.8
+	state = MOVE
+	
+func move_with_velocity():
+	velocity = move_and_slide(velocity)
+	
 
 func move_state(delta):
 	input_vector = input_vector.normalized()	
 
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -62,4 +81,4 @@ func move_state(delta):
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
-	velocity = move_and_slide(velocity)
+	move_with_velocity()
